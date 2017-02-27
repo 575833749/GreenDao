66b2b6569f5ce459a59b6aed5e8d5e22f13012d8
zwz <575833749@qq.com>
2017-02-27 18:09:01 +0800

    Initial commit

--- app/src/main/java/zwz/greendao/MainActivity.java
+++ app/src/main/java/zwz/greendao/MainActivity.java
@@ -23,28 +23,21 @@ public class MainActivity extends AppCompatActivity {
         Animal animal=new Animal();
         animal.setAnimalName("animal");
         animalDao.save(animal);
-
         animal=new Animal();
         animal.setAnimalName("animal2");
         animalDao.save(animal);
-
         User user=new User();
         user.setAge(1);
         user.setName("a");
-        user.setTest("test");
         userDao.save(user);
         user=new User();
-
-
         user.setAge(33);
         user.setName("b");
         userDao.save(user);
-
         List<User> users = userDao.loadAll();
         for (int i = 0; i < users.size(); i++) {
             Log.d("vivi","------------user-----="+users.get(i).toString());
         }
-
         List<Animal> animals = animalDao.loadAll();
         for (int i = 0; i < animals.size(); i++) {
             Log.d("vivi","---------------animals-----="+animals.get(i).toString());
